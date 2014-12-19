e82a5582f8169767950b4dc8fe36aacf73e9ed39
sudheesh001 <sudheesh1995@outlook.com>
2014-12-19 23:16:59 +0530

    Added parsing the URL, Learnt about regcomp and regexec in C

--- 5. Web Client/webclient.c
+++ 5. Web Client/webclient.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <regex.h>
+#include <arpa/inet.h>
 
 int main(int argc, char** argv)
 {
@@ -12,5 +19,22 @@ int main(int argc, char** argv)
 	}
 
 	/* Need to take an input URL and parse it */
+	regex_t urlPreg;
+	regex_t errorResponse;
+	regex_t okResponse;
+	char *string=argv[1];	
+	if(regcomp(&urlPreg,"(http://)([^/:. ]+(\\.[^/:. ]+)+)(/?.*)",0 | REG_ICASE | REG_EXTENDED)!=0)
+		puts("URL regex compilation failed");					
+	if(regcomp(&errorResponse,"^HTTP/1\\.[0-9]+ [34][0-9][0-9]",0 | REG_ICASE | REG_EXTENDED | REG_NOSUB)!=0)
+		puts("Error response regex failed to compile");		
+	if(regcomp(&okResponse,"^HTTP/1\\.[0-9]+ 2[0-9][0-9]",0 | REG_ICASE | REG_EXTENDED | REG_NOSUB)!=0)
+		puts("Ok response regex failed to compile");	
+	regmatch_t matchedArray[6];
+	if(regexec(&urlPreg,string,(size_t)6,matchedArray,0)==REG_NOMATCH)
+	{
+		puts("Please enter a proper URL");			
+		return 1;
+	}
+	
 	return 0;
 }

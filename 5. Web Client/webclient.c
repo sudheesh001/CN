e3e6a5e485889712a63c82298e5ad89db895a870
sudheesh001 <sudheesh1995@outlook.com>
2014-12-20 13:44:12 +0530

    Adding hostname, connect() and address information

--- 5. Web Client/webclient.c
+++ 5. Web Client/webclient.c
@@ -35,6 +35,33 @@ int main(int argc, char** argv)
 		puts("Please enter a proper URL");			
 		return 1;
 	}
+
+	char hostName[1024];
+	char requestFileName[1024];
+	char saveFileName[1024];
+	strncpy(hostName,&string[matchedArray[2].rm_so],(matchedArray[2].rm_eo-matchedArray[2].rm_so));
+	hostName[(matchedArray[2].rm_eo-matchedArray[2].rm_so)]='\0';
+	strncpy(requestFileName,&string[matchedArray[4].rm_so],(matchedArray[4].rm_eo-matchedArray[4].rm_so));
+	requestFileName[(matchedArray[4].rm_eo-matchedArray[4].rm_so)]='\0';
+	printf("The Host is :%s\n",hostName);
+	printf("The requested filename is:%s\n",requestFileName);
+	
+	struct addrinfo hints;	
+	struct addrinfo *result=0;	
+	hints.ai_family=AF_INET;
+	hints.ai_socktype=SOCK_STREAM;
+	hints.ai_protocol=0;	
+	if(getaddrinfo((const char *)hostName,0,&hints,&result)!=0)		// get the address for name without the protocol part			
+	{	perror("No name record exists for the host: "); return 1;	}
+	
+	((struct sockaddr_in *)(result->ai_addr))->sin_port=htons(80);	//set the port no to 80		
+	
+	int res = connect(sock, (struct sockaddr*)(result->ai_addr),result->ai_addrlen);	//try to connect
+	if(res < 0) {
+		perror("Error connecting: ");
+		exit(1);
+	}
+	printf("Connected to %s\n", hostName);
 	
 	return 0;
 }
